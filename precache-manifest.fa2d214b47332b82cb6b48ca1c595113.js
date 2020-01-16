e577228decf164ec0e2bd84105a879a5e19001d6
fernandaamancio <fernanda-agomes@hotmail.com>
2020-01-16 03:21:49 -0300

    Updates

---
+++ precache-manifest.fa2d214b47332b82cb6b48ca1c595113.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c602490ebf66170c348e51e8386bca",
+    "url": "/feamancio/feamancio.github.io/index.html"
+  },
+  {
+    "revision": "126996e4d09e986aee66",
+    "url": "/feamancio/feamancio.github.io/static/css/main.095372d9.chunk.css"
+  },
+  {
+    "revision": "a50e14775f27c93aecac",
+    "url": "/feamancio/feamancio.github.io/static/js/2.6f0d3e38.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/feamancio/feamancio.github.io/static/js/2.6f0d3e38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "126996e4d09e986aee66",
+    "url": "/feamancio/feamancio.github.io/static/js/main.2094083f.chunk.js"
+  },
+  {
+    "revision": "0fec25a2b8d7a501aec8",
+    "url": "/feamancio/feamancio.github.io/static/js/runtime-main.2e608151.js"
+  },
+  {
+    "revision": "cd7b36c8fec16208ebd29fac233f5fe3",
+    "url": "/feamancio/feamancio.github.io/static/media/background.cd7b36c8.png"
+  },
+  {
+    "revision": "bb54ad9520cd15ae341960461ed5faab",
+    "url": "/feamancio/feamancio.github.io/static/media/portbackground.bb54ad95.png"
+  },
+  {
+    "revision": "dbd7f69a8b3c359c0cd0a1d475327297",
+    "url": "/feamancio/feamancio.github.io/static/media/profile.dbd7f69a.png"
+  },
+  {
+    "revision": "de2e5c570d35509ae6528646afc01cce",
+    "url": "/feamancio/feamancio.github.io/static/media/skills1.de2e5c57.png"
+  },
+  {
+    "revision": "3f72ec99e58fffb4d7d74cd450b68c48",
+    "url": "/feamancio/feamancio.github.io/static/media/skills5.3f72ec99.png"
+  },
+  {
+    "revision": "8cd9d39a0f4cf513a78159496fd12b4d",
+    "url": "/feamancio/feamancio.github.io/static/media/vertra.8cd9d39a.png"
+  }
+]);
